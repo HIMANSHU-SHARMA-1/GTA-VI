fdf9b0c397ecdb1373f663b8cc211b5f16163edd
HIMANSHU-SHARMA-1 <himanshusharma20969@gmail.com>
2025-11-09 23:31:16 +0530

    updated font style and added pricedown.otf file for gta themed font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'pricedown': ['pricedown', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
